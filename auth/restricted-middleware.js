4d4f122f0fde94e58d46d105c72ddf429cd63fd4
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-03-24 16:35:53 -0700

    debugging in progress -- session wont end

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,8 @@
+module.exports = (req, res, next) => {
+  console.log('req.session');
+  if (req.session && req.session.user) {
+    next();
+  } else {
+    res.status(401).json({ message: 'please login to continue' });
+  }
+};
